934044e148f82bfb24cacf3fc55a5f198bbe4195
MD Titumir Anan <titumiranan.gtc@gmail.com>
2024-01-14 10:07:20 +0600

    update

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -5,7 +5,8 @@ import { ProductModel } from "./product.model";
 
 export const postProduct = async (req: Request, res: Response) => {
     const product = req.body
-    await ProducttoDb(product)
+    const data = await ProducttoDb(product)
+    return res.status(200).json({ message: 'success', data });
 }
 export const getProduct = async (req: Request, res: Response) => {
     const products = await ProductModel.find()
@@ -22,12 +23,14 @@ export const getsingleProduct = async (req: Request, res: Response) => {
     }
 }
 export const deletProduct = async (req: Request, res: Response) => {
+
     try {
         const deletedProduct = await ProductModel.findByIdAndDelete({ _id: req.params.id })
-        console.log(deletedProduct)
+
         if (!deletedProduct) {
             return res.status(404).json({ message: "product is not found" })
         }
+        return res.status(200).json({ message: 'deleted', deletedProduct });
     } catch (err: any) {
         res.status(500).json({ message: err.message });
     }
@@ -35,12 +38,16 @@ export const deletProduct = async (req: Request, res: Response) => {
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const product = req.body;
-    const updatedProduct = await ProductModel.findByIdAndUpdate({ _id: id, product, new: true })
+    const productId = req.params.id;
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+        productId,
+        req.body,
+        { new: true, runValidators: true }
+    );
     if (!updatedProduct) {
         return res.status(404).json({ message: 'Product not found' });
     }
-    res.json(updatedProduct);
+    return res.status(200).json({ message: 'Product Update', updatedProduct });
 
 }
