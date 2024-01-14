4954f31fcc07aeabd05cc14d75967a36b0bbb578
MD Titumir Anan <titumiranan.gtc@gmail.com>
2024-01-15 03:19:37 +0600

    update-2

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,22 +7,28 @@ import paymentRoutes from './modules/payment/pay.routes'
 import 'dotenv/config'
 import jwtRoutes from './modules/jwt/jwt.routes'
 const app = express()
-app.use(cors())
-
-app.use(express.json({ limit: '10mb' })); // Adjust the limit accordingly
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use('/user', userRoutes)
-app.use('/products', productRoutes)
-app.use('/cart', cartRoutes)
-app.use('/payment', paymentRoutes)
-app.use('/jwt', jwtRoutes)
-
-
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    next();
+    app.use(cors())
+
+    app.use(express.json({ limit: '10mb' })); // Adjust the limit accordingly
+    app.use(express.urlencoded({ limit: '10mb', extended: true }));
+    app.use('/user', userRoutes)
+    app.use('/products', productRoutes)
+    app.use('/cart', cartRoutes)
+    app.use('/payment', paymentRoutes)
+    app.use('/jwt', jwtRoutes)
+
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://chamrabari-backend.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 app.get('/', (req, res) => {
     res.json("server is running")
